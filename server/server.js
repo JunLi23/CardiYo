cdc11ca7a26da27a6652efc06da218c1af04f237
Junqing Li <jl2123@hw.ac.uk>
2026-02-16 12:44:11 +0000

    fix server.js

---
+++ server/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config({ path: './config.env' });
+
+import express from "express";
+import cors from "cors";
+import records from "./routes/record.js";
+import dotenv from "dotenv";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/record", records);
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
