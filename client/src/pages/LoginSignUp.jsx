f114f8fdbb3e66e1386e29e90ef6c1fc56d1ca2d
Seán Burns <sb2141@hw.ac.uk>
2026-03-04 15:59:40 +0000

    dynamic signup buttons

--- client/src/pages/LoginSignUp.jsx
+++ client/src/pages/LoginSignUp.jsx
@@ -5,10 +5,13 @@ import emailIcon from '../assets/email.png';
 import passwordIcon from '../assets/password.png';
 
 const LoginSignUp = () => {
+
+    const [action,setAction] = useState("Sign Up");
+
   return (
     <div className='container'>
         <div className="header">
-            <div className="text">Sign Up</div>
+            <div className="text">{action}</div>
             <div className="underline"></div>
         </div>
         <div className="inputs">
@@ -26,8 +29,8 @@ const LoginSignUp = () => {
             </div>
         </div>
         <div className="submit_container">
-            <div className="submit">Sign Up</div>
-            <div className="submit">Login</div>
+            <div className={action==="Login"?"submit grey":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+            <div className={action==="Sign Up"?"submit grey":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
         </div>
     </div>
 
