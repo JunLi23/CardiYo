5bc5e3f3574ddcae12a29ed4a8402c5ca247ad25
JunLi <jl2123@hw.ac.uk>
2026-03-05 12:19:16 +0000

    adjusted hamburger, navbar fixed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -65,36 +65,17 @@ const NavbarComponent = () => {
         </div>
 
         {/* Mobile hamburger */}
-        <div className="lg:hidden">
-          <IconButton
-            variant="text"
-            className="flex items-center justify-center h-10 w-10 p-2 text-white"
-            ripple={false}
+       <div className="lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            className="h-6 w-6 text-white cursor-pointer"
+            stroke="currentColor"
+            strokeWidth={2}
             onClick={() => setOpenNav(!openNav)}
           >
-            {openNav ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                className="h-6 w-6"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                className="h-6 w-6"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-              </svg>
-            )}
-          </IconButton>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
         </div>
       </div>
 
