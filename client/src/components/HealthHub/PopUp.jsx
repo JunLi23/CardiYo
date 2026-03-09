e2456141138256718219d25f142a0fe0764675bd
JunLi <jl2123@hw.ac.uk>
2026-03-09 12:49:20 +0000

    Health Hub Components

---
+++ client/src/components/HealthHub/PopUp.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const HealthProv_PopUp = () => {
+    return (
+        <div className='fixed inset-0 background-color: #E6E7D5 bg-opacity-25 backdrop-blur-sm flex justify-center items-center'>
+            <div className='w-[600px]'>
+                <div className='bg-white p-2 rounded'>
+                <h3 class="mb-4 text-x1 front-medium text-gray-900">
+                    Healthcare Provider
+                </h3>
+
+                </div>
+            </div>
+        </div>
+    )
+
+
+}
+
+export default HealthProv_PopUp
--- client/src/pages/HealthHub.jsx
+++ client/src/pages/HealthHub.jsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import PopUp from "../components/HealthHub/PopUp";
+import { Fragment } from "react";
 
 const HealthHub = () => {
   return (
     <>
+      <Fragment>
       <Navbar />
       <div style={{ padding: "2rem" }}>
         <h1>Health Hub Page</h1>
       </div>
+      {/* </PopUp /> */}
+      </Fragment>
     </>
   );
 };
