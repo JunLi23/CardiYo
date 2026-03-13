2fdc8350c9aa3af6c8388ecacf153285e01a3171
ks2126 <kieran.sharma76@hotmail.com>
2026-03-13 13:29:13 +0000

    scroll to top when openinng faq page (mainly mobile issue)

--- client/src/pages/FAQ.jsx
+++ client/src/pages/FAQ.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -42,6 +42,10 @@ const faqs = [
 const FAQ = () => {
   const [openIndex, setOpenIndex] = useState(null);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   const toggle = (index) => {
     setOpenIndex(openIndex === index ? null : index);
