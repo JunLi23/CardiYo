b9581a200b69d4f3e0a04f51393a5e3fc23b1933
JunLi <jl2123@hw.ac.uk>
2026-03-17 17:55:11 +0000

    added footer to the dahsboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -7,35 +7,36 @@ import ProgressDisplay from "../components/dashboardComponents/ProgressDisplay";
 import NotifacationDisplay from "../components/dashboardComponents/NotificationCentre";
 import MountainImg from "../assets/Everest.svg"
 import MountainPath from "../components/dashboardComponents/MountainPath";
+import Footer from "../components/Footer";
 
 const Dashboard = ()=> {
     
     return (
         <>
-
-        <Navbar />
-            <div className={styles.dashboard}>
-                <div className={styles.mountainSection}>
-                    <img src={MountainImg} alt="Mountain" className={styles.mountainImg} />
-                    <div className={styles.cloudLeft}>
-                        <BioMarker index={0} />                    
+            <Navbar />
+                <div className={styles.dashboard}>
+                    <div className={styles.mountainSection}>
+                        <img src={MountainImg} alt="Mountain" className={styles.mountainImg} />
+                        <div className={styles.cloudLeft}>
+                            <BioMarker index={0} />                    
+                        </div>
+                        <div className={styles.cloudCenter}>
+                            <BioMarker index={1} />                    
+                        </div>
+                        <div className={styles.cloudRight}>
+                            <BioMarker index={2} />
+                        </div>
                     </div>
-                    <div className={styles.cloudCenter}>
-                        <BioMarker index={1} />                    
+                    <div className={styles.middleSection}>
+                        <ProgressDisplay />
+                        <AchievementDisplay />
                     </div>
-                    <div className={styles.cloudRight}>
-                        <BioMarker index={2} />
+                    <div className={styles.bottomSection}>
+                        <NotifacationDisplay />
                     </div>
                 </div>
-                <div className={styles.middleSection}>
-                    <ProgressDisplay />
-                    <AchievementDisplay />
-                </div>
-                <div className={styles.bottomSection}>
-                    <NotifacationDisplay />
-                </div>
-            </div>
-        </>
+            <Footer />
+            </>
     );
 }
 export default Dashboard;
