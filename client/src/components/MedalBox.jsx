b42018ccc3fe6c0a3cbcfd7d2373731b87c21b95
Chloe Barron <86964931+CB0615@users.noreply.github.com>
2026-03-18 17:09:40 +0000

    Change MedalBox only to display medals when earned

--- client/src/components/MedalBox.jsx
+++ client/src/components/MedalBox.jsx
@@ -2,45 +2,49 @@
 import goldMedel from "../assets/gBadge.png";
 import greyMedel from "../assets/greyBadge.png";
 
-const MedalIcon = ({earned, label}) => {
+const MedalIcon = ({name}) => {
   return (
     <div>
-      <img src={earned ? goldMedel : greyMedel} className="w-15 h-15 md:w-20 md:h-20" />
-      <div className="mt-1 mb-3 text-sm"> {label} </div>
+      <img src={goldMedel} className="w-15 h-15 md:w-20 md:h-20" />
+      <div className="mt-1 mb-3 text-sm"> {name} </div>
     </div>
   );
 };
 
 const MedalBox = () => {
-    const medals = [
-      { id: 1, label: "Aoraki", earned: false},
-      { id: 2, label: "Ben Nevis", earned: false},
-      { id: 3, label: "Chimborazo", earned: false},
-      { id: 4, label: "Eiger", earned: false},
-      { id: 5, label: "Fuji", earned: true},
-      { id: 6, label: "Matterhorn", earned: false},
-      { id: 7, label: "Annapurna", earned: false},
-      { id: 8, label: "Kinabalu", earned: true},
-      { id: 9, label: "Everest", earned: true},
-    ];
+  const medals = [
+    { id: 1, name: "Aoraki", earned: true},
+    { id: 2, name: "Ben Nevis", earned: true},
+    { id: 3, name: "Chimborazo", earned: true},
+    { id: 4, name: "Eiger", earned: false},
+    { id: 5, name: "Fuji", earned: false},
+    { id: 6, name: "Matterhorn", earned: true},
+    { id: 7, name: "Annapurna", earned: true},
+    { id: 8, name: "Kinabalu", earned: false},
+    { id: 9, name: "Everest", earned: true},
+  ];
 
-    return (
+  const completedMedals = medals.filter((m) => m.earned);
+
+  return (
     <>
-        {/* Achievements Section */}
-        <div className="bg-[#5E806D] border-8 border-[#3C5246]">
+      {/* Achievements Section */}
+      <div className="bg-[#5E806D] border-8 border-[#3C5246]">
             
-            {/* Medal Section */}
-            <h2 className="text-xl pt-2">Medals</h2>
-            <div className="grid grid-cols-3 justify-items-center mt-3">
-                {medals.map(({ id, earned, label }) => (<MedalIcon key={id} earned={earned} label={label}/>))}
-            </div>
-            
-            {/* Trophy Section */}
-            <div className="border-t-8 border-[#3C5246]">
-                <h2 className="text-xl pt-2">Trophies</h2>
-                <div className="m-3 text-[#DCDCDC]"> No Trophies Earned </div> 
-            </div>
+        {/* Medal Section */}
+        <h2 className="text-xl pt-2">Medals</h2>
+
+        <div className="grid grid-cols-3 justify-items-center mt-3">
+          {completedMedals.map((medal) => (<MedalIcon key={medal.id} name={medal.name} image={medal.goldMedel}/>))}
         </div>
+              
+        {/* Trophy Section */}
+        <div className="border-t-8 border-[#3C5246]">
+          <h2 className="text-xl pt-2">Trophies</h2>
+
+          <div className="m-3 text-[#DCDCDC]"> No Trophies Earned </div> 
+        </div>
+      </div>
     </>
   );
 };
--- client/src/components/MountainBox.jsx
+++ client/src/components/MountainBox.jsx
@@ -10,7 +10,6 @@ import mountKinabalu from "../assets/mount-kinabalu.png";
 import everest from  "../assets/Everest.svg"
 
 const MountainIcon = ({name, image}) => {
-  
   return (
     <div>
       <img src={image} alt={name} className="w-15 h-15 md:w-20 md:h-20 object-cover rounded-full" />
@@ -20,7 +19,7 @@ const MountainIcon = ({name, image}) => {
 };
 
 const MountainBox = () => {
-    const mountains = [
+  const mountains = [
     { id: 1, name: "Aoraki", image: aoraki, completed: true},
     { id: 2, name: "Ben Nevis", image: benNevis, completed: true},
     { id: 3, name: "Chimborazo", image: chimborazo, completed: true},
@@ -30,18 +29,20 @@ const MountainBox = () => {
     { id: 7, name: "Annapurna", image: mountAnnapurna, completed: true},
     { id: 8, name: "Kinabalu", image: mountKinabalu, completed: false},
     { id: 9, name: "Everest", image: everest, completed: true},
-    ];
+  ];
 
-const completedMountains = mountains.filter((m) => m.completed);
+  const completedMountains = mountains.filter((m) => m.completed);
+
   return (
     <>
-        {/* Mountain Section */}
-        <div className="bg-[#5E806D] border-8 border-[#3C5246] pb-5 md:p-0 md:pb-0">
-            <h2 className="text-xl pt-2"> Mountains Completed </h2>
-            <div className="grid grid-cols-3 justify-items-center mt-3">
-                {completedMountains.map((mountain) => (<MountainIcon key={mountain.id} name={mountain.name} image={mountain.image}/>))}
-            </div>
-        </div>
+      {/* Mountain Section */}
+      <div className="bg-[#5E806D] border-8 border-[#3C5246] pb-5 md:p-0 md:pb-0">
+          <h2 className="text-xl pt-2"> Mountains Completed </h2>
+          
+          <div className="grid grid-cols-3 justify-items-center mt-3">
+              {completedMountains.map((mountain) => (<MountainIcon key={mountain.id} name={mountain.name} image={mountain.image}/>))}
+          </div>
+      </div>
     </>
   );
 };
